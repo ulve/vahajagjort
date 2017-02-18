219697658fcdd0923474d62138613649c02eea04
Olov Johansson <johansson.olov@gmail.com>
2017-02-18 22:29:08 +0100

    A little cleanup

--- vahajagjort/vahajagjort.fs
+++ vahajagjort/vahajagjort.fs
@@ -14,67 +14,33 @@ module Program =
     open System
         
     type BlockingItem = { Id: int; Person:string; Text:string; Date:DateTime }
-    type DoingItem = { Id: int; Person:string; Text:string; Date:DateTime }   
-    type DoneItem  = { Id: int; Person:string; Text:string; Date:DateTime }
+    type DoingItem    = { Id: int; Person:string; Text:string; Date:DateTime }   
+    type DoneItem     = { Id: int; Person:string; Text:string; Date:DateTime }
+
+    let initStorage path = 
+        match Serializer.deserialize path with
+        | Some a -> a
+        | None -> new Dictionary<int, 'a>()  
+
+    let setupWebPart<'a> path name create getId =         
+        let storage = initStorage path        
+        let serializer = Serializer.serialize path
+
+        restish name {
+            GetAll = fun () -> DictionaryStorage.getAll storage
+            Create = DictionaryStorage.create storage create serializer
+            Update = DictionaryStorage.updateItem storage create getId serializer
+            Delete = DictionaryStorage.deleteItem storage serializer
+            GetById = DictionaryStorage.getItem storage
+            UpdateById = DictionaryStorage.updateItemById storage create serializer
+            IsExists = DictionaryStorage.isExists storage
+        }
 
     [<EntryPoint>]
     let main _ =                
-        let donePath = "./done.json"
-        let doneStorage = match Serializer.deserialize donePath with
-                          | Some a -> a
-                          | None -> new Dictionary<int, DoneItem>() 
+        let doneWebPart = setupWebPart<DoneItem> "./done.json" "done" (fun a b -> {Id = a; Person = b.Person; Text = b.Text; Date = DateTime.Now}) (fun a -> a.Id)
+        let doingWebPart = setupWebPart<DoingItem> "./doing.json" "done" (fun a b -> {Id = a; Person = b.Person; Text = b.Text; Date = DateTime.Now}) (fun a -> a.Id)
+        let blockingWebPart = setupWebPart<BlockingItem> "./blocking.json" "done" (fun a b -> {Id = a; Person = b.Person; Text = b.Text; Date = DateTime.Now}) (fun a -> a.Id)
         
-        let doneCreate a b = {DoneItem.Id = a; Person = b.Person; Text = b.Text; Date = DateTime.Now}
-        let doneGetId a = a.Id        
-        let doneSerializer = Serializer.serialize donePath
-
-        let doneWebPart = restish "done" {
-            GetAll = fun () -> DictionaryStorage.getAll doneStorage
-            Create = DictionaryStorage.create doneStorage doneCreate doneSerializer
-            Update = DictionaryStorage.updateItem doneStorage doneCreate doneGetId doneSerializer
-            Delete = DictionaryStorage.deleteItem doneStorage doneSerializer
-            GetById = DictionaryStorage.getItem doneStorage
-            UpdateById = DictionaryStorage.updateItemById doneStorage doneCreate doneSerializer
-            IsExists = DictionaryStorage.isExists doneStorage
-        }
-
-        let doingPath = "./doing.json"
-        let doingStorage = match Serializer.deserialize doingPath with
-                           | Some a -> a
-                           | None -> new Dictionary<int, DoingItem>() 
-        
-        let doingCreate a (b:DoingItem) = {DoingItem.Id = a; Person = b.Person; Text = b.Text; Date = DateTime.Now}
-        let doingGetId (a:DoingItem) = a.Id
-        let doingSerializer = Serializer.serialize doingPath
-
-        let doingWebPart = restish "doing" {
-            GetAll = fun () -> DictionaryStorage.getAll doingStorage
-            Create = DictionaryStorage.create doingStorage doingCreate doingSerializer
-            Update = DictionaryStorage.updateItem doingStorage doingCreate doingGetId doingSerializer
-            Delete = DictionaryStorage.deleteItem doingStorage doingSerializer
-            GetById = DictionaryStorage.getItem doingStorage
-            UpdateById = DictionaryStorage.updateItemById doingStorage doingCreate doingSerializer
-            IsExists = DictionaryStorage.isExists doingStorage
-        } 
-        
-        let blockingPath = "./blocking.json"
-        let blockingStorage = match Serializer.deserialize blockingPath with
-                              | Some a -> a
-                              | None -> new Dictionary<int, BlockingItem>() 
-        
-        let blockingCreate a (b:BlockingItem) = {BlockingItem.Id = a; Person = b.Person; Text = b.Text; Date = DateTime.Now}
-        let blockingGetId (a:BlockingItem) = a.Id
-        let blockingSerializer = Serializer.serialize blockingPath
-
-        let blockingWebPart = restish "blocking" {
-            GetAll = fun () -> DictionaryStorage.getAll blockingStorage
-            Create = DictionaryStorage.create blockingStorage blockingCreate blockingSerializer
-            Update = DictionaryStorage.updateItem blockingStorage blockingCreate blockingGetId blockingSerializer
-            Delete = DictionaryStorage.deleteItem blockingStorage blockingSerializer
-            GetById = DictionaryStorage.getItem blockingStorage
-            UpdateById = DictionaryStorage.updateItemById blockingStorage blockingCreate blockingSerializer
-            IsExists = DictionaryStorage.isExists blockingStorage
-        } 
-
         startWebServer defaultConfig (choose [doneWebPart; doingWebPart; blockingWebPart])
         0
